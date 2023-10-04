c535b544937e3c779a87c97fe1a55cf476595851
Mi Kee <123068360+balineseleaf@users.noreply.github.com>
2023-10-04 13:26:23 +0300

    Create avro_example_2.py
    add avro example

---
+++ task_1/avro_example_2.py
@@ -0,0 +1,18 @@
+import avro.schema
+from avro.datafile import DataFileReader, DataFileWriter
+from avro.io import DatumReader, DatumWriter
+
+# Загрузка схемы из файла
+schema = avro.schema.Parse(open("person.avsc").read())
+
+# Сериализация данных
+writer = DataFileWriter(open("people.avro", "wb"), DatumWriter(), schema)
+writer.append({"name": "Alice", "age": 30, "address": "123 Main St"})
+writer.append({"name": "Bob", "age": 25, "address": "456 Elm St"})
+writer.close()
+
+# Десериализация данных
+reader = DataFileReader(open("people.avro", "rb"), DatumReader())
+for person in reader:
+    print(person)
+reader.close()
