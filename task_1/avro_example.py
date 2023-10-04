0b1ed54ca24690ebc8684e6707429bc0deed11f1
darkobzar <86746290+darkobzar@users.noreply.github.com>
2023-10-04 12:35:57 +0300

    Add files via upload
    add avro example

---
+++ task_1/avro_example.py
@@ -0,0 +1,19 @@
+
+import avro.schema
+from avro.datafile import DataFileReader, DataFileWriter
+from avro.io import DatumReader, DatumWriter
+
+schema = avro.schema.parse(open("user.avsc", "rb").read())
+
+
+writer = DataFileWriter(open("users.avro", "wb"), DatumWriter(), schema)
+writer.append({"name": "Ivan", "age": 10, "is_man": True, "height": 100.5, "pet": {"kind": "dog", "age": 11.}, "grades": [5, 4, 3], "add_data": {"0": {"abc": [1, 2, 3], "def": [123]}, "1": {"hij": [0]}}})
+writer.close()
+
+reader = DataFileReader(open("users.avro", "rb"), DatumReader())
+for user in reader:
+    print(user)
+reader.close()
+
+
+
