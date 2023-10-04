bab6bf3079e13b9940631ff3a2a121a5a9f1a1a9
darkobzar <86746290+darkobzar@users.noreply.github.com>
2023-10-04 12:37:52 +0300

    Update avro_example.py

--- task_1/avro_example.py
+++ task_1/avro_example.py
@@ -3,10 +3,10 @@ import avro.schema
 from avro.datafile import DataFileReader, DataFileWriter
 from avro.io import DatumReader, DatumWriter
 
-schema = avro.schema.parse(open("user.avsc", "rb").read())
+schema = avro.schema.parse(open("schema.avsc", "rb").read())
 
 
-writer = DataFileWriter(open("users.avro", "wb"), DatumWriter(), schema)
+writer = DataFileWriter(open("schema.avro", "wb"), DatumWriter(), schema)
 writer.append({"name": "Ivan", "age": 10, "is_man": True, "height": 100.5, "pet": {"kind": "dog", "age": 11.}, "grades": [5, 4, 3], "add_data": {"0": {"abc": [1, 2, 3], "def": [123]}, "1": {"hij": [0]}}})
 writer.close()
 
